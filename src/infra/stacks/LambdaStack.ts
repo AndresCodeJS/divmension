57e9dfd8a420ce672f9155a1f93a4a6961958dc1
Andres <andres2661991@gmail.com>
2024-08-18 19:42:12 -0400

    set timeout on lambdaUser

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,59 +1,58 @@
-import { Stack, StackProps } from 'aws-cdk-lib'
-import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
-import { ITable } from 'aws-cdk-lib/aws-dynamodb';
-import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { Construct } from 'constructs';
-import { join } from 'node:path';
-
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
+import { ITable } from "aws-cdk-lib/aws-dynamodb";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+import { join } from "node:path";
 
 interface LambdaStackProps extends StackProps {
-    devmensionTable: ITable,
-    gsi1Name: string
+  devmensionTable: ITable;
+  gsi1Name: string;
 }
 
-
 export class LambdaStack extends Stack {
+  public readonly usersLambdaIntegration: LambdaIntegration;
 
-    public readonly usersLambdaIntegration: LambdaIntegration
+  constructor(scope: Construct, id: string, props: LambdaStackProps) {
+    super(scope, id, props);
 
-    constructor(scope: Construct, id: string, props: LambdaStackProps) {
-        super(scope, id, props)
+    const usersLambda = new NodejsFunction(this, "usersLambda", {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "handler",
+      entry: join(__dirname, "..", "..", "services", "users", "handler.ts"),
+      environment: {
+        TABLE_NAME: props.devmensionTable.tableName,
+        TABLE_GSI1_NAME: props.gsi1Name,
+        SECRET_KEY: "DIVMENSION_SECRET_PW_KEY",
+        JWT_SECRET: "JWT_SECRET_CODE",
+      },
+      bundling: {
+        nodeModules: ["bcryptjs", "jsonwebtoken"],
+      },
+      timeout: Duration.seconds(6)
+    });
 
-        const usersLambda = new NodejsFunction(this, 'usersLambda', {
-            runtime: Runtime.NODEJS_18_X,
-            handler: 'handler',
-            entry: (join(__dirname, '..', '..', 'services', 'users', 'handler.ts')),
-            environment: {
-                TABLE_NAME: props.devmensionTable.tableName,
-                TABLE_GSI1_NAME:props.gsi1Name ,
-                SECRET_KEY: 'DIVMENSION_SECRET_PW_KEY',
-                JWT_SECRET: 'JWT_SECRET_CODE'
-            },
-              bundling: {
-                  nodeModules: ['bcryptjs', 'jsonwebtoken'],
-              },
+    usersLambda.addToRolePolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: [
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:Scan",
+          "dynamodb:Query",
+        ],
+        resources: [
+          props.devmensionTable.tableArn,
+          `${props.devmensionTable.tableArn}/index/${props.gsi1Name}`,
+        ],
+      })
+    );
 
-        })
-
-        usersLambda.addToRolePolicy(new PolicyStatement({
-            effect: Effect.ALLOW,
-            actions: [
-                'dynamodb:PutItem',
-                'dynamodb:DeleteItem',
-                'dynamodb:GetItem',
-                'dynamodb:UpdateItem',
-                'dynamodb:Scan',
-                'dynamodb:Query'
-            ],
-            resources: [props.devmensionTable.tableArn,
-                `${props.devmensionTable.tableArn}/index/${props.gsi1Name}`
-            ]
-
-        }))
-
-        /*   placesLambda.addToRolePolicy(new PolicyStatement({
+    /*   placesLambda.addToRolePolicy(new PolicyStatement({
               effect: Effect.ALLOW,
               actions: [
                   's3:ListAllMyBuckets',
@@ -62,7 +61,6 @@ export class LambdaStack extends Stack {
               resources: ['*'] //bad practice
           })) */
 
-        this.usersLambdaIntegration = new LambdaIntegration(usersLambda)
-    }
-
+    this.usersLambdaIntegration = new LambdaIntegration(usersLambda);
+  }
 }
