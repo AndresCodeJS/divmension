1ee29a08011f242d6b26aede6b68c548506a00f2
Andres <andres2661991@gmail.com>
2024-10-03 16:47:10 -0400

    add policies to ApiChatStack

--- src/infra/stacks/ApiChatStack.ts
+++ src/infra/stacks/ApiChatStack.ts
@@ -1,69 +1,88 @@
-import { aws_apigatewayv2 as apigatewayv2, CfnOutput, Duration } from 'aws-cdk-lib';
+import {
+  aws_apigatewayv2 as apigatewayv2,
+  CfnOutput,
+  Duration,
+} from 'aws-cdk-lib';
 import { aws_iam as iam } from 'aws-cdk-lib';
 import { WebSocketLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
 import { Construct } from 'constructs';
-import { Stack, StackProps } from 'aws-cdk-lib'
-import { Runtime } from "aws-cdk-lib/aws-lambda";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import { join } from "node:path";
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { join } from 'node:path';
 
-    interface ApiStackProps extends StackProps {
-        /* lambdaChatIntegration:  WebSocketLambdaIntegration, */
-      /*   usersLambdaIntegration: LambdaIntegration, */
-        /* userPool: IUserPool */
-    }
-    
-    export class ApiChatStack extends Stack {
-    
-        constructor(scope: Construct, id: string, props?: ApiStackProps) {
-            super(scope, id, props)
+interface ApiStackProps extends StackProps {
+  /* lambdaChatIntegration:  WebSocketLambdaIntegration, */
+  /*   usersLambdaIntegration: LambdaIntegration, */
+  /* userPool: IUserPool */
+}
 
-            //FUNCION LAMBDA A SER EJECUTADA POR EL WEBSOCKET
-            const lambdaChat = new NodejsFunction(this, "lambdaChat", {
-                runtime: Runtime.NODEJS_18_X,
-                handler: "handler",
-                entry: join(__dirname, "..", "..", "services", "chat", "handler.ts"), 
-                environment: {
-            /*       TABLE_NAME: props.devmensionTable.tableName,
+export class ApiChatStack extends Stack {
+  constructor(scope: Construct, id: string, props?: ApiStackProps) {
+    super(scope, id, props);
+
+    //FUNCION LAMBDA A SER EJECUTADA POR EL WEBSOCKET
+    const lambdaChat = new NodejsFunction(this, 'lambdaChat', {
+      runtime: Runtime.NODEJS_18_X,
+      handler: 'handler',
+      entry: join(__dirname, '..', '..', 'services', 'chat', 'handler.ts'),
+      environment: {
+        /*       TABLE_NAME: props.devmensionTable.tableName,
                   TABLE_GSI1_NAME: props.gsi1Name,
                   SECRET_KEY: "DIVMENSION_SECRET_PW_KEY",
                   JWT_SECRET: "JWT_SECRET_CODE",
                   S3_ACCESS_ROLE_NAME: props.s3AccessRole.roleName, */
-                  ACCOUNT_ID: "339712893600",
-                  REGION: "us-east-1",
-                },
-           /*      bundling: {
+        ACCOUNT_ID: '339712893600',
+        REGION: 'us-east-1',
+      },
+      /*      bundling: {
                   nodeModules: ["bcryptjs", "jsonwebtoken", "ulid"],
                 }, */
-                timeout: Duration.seconds(6),
-              });
+      timeout: Duration.seconds(6),
+    });
 
-              //EJECUCIONES DE LAMBDA CUANDO OCURRE UNA CONEXION O DESCONEXION
-              const webSocketApi = new apigatewayv2.WebSocketApi(this, 'DivmensionChatApi',{
-                connectRouteOptions: {
-                  integration: new WebSocketLambdaIntegration('ConnectIntegration', lambdaChat),
-                },
-                disconnectRouteOptions: {
-                  integration: new WebSocketLambdaIntegration('DisconnectIntegration', lambdaChat),
-                },
-                defaultRouteOptions: {
-                  integration: new WebSocketLambdaIntegration('DefaultIntegration', lambdaChat),
-                },
-              });
+    //EJECUCIONES DE LAMBDA CUANDO OCURRE UNA CONEXION O DESCONEXION
+    const webSocketApi = new apigatewayv2.WebSocketApi(
+      this,
+      'DivmensionChatApi',
+      {
+        connectRouteOptions: {
+          integration: new WebSocketLambdaIntegration(
+            'ConnectIntegration',
+            lambdaChat
+          ),
+        },
+        disconnectRouteOptions: {
+          integration: new WebSocketLambdaIntegration(
+            'DisconnectIntegration',
+            lambdaChat
+          ),
+        },
+        defaultRouteOptions: {
+          integration: new WebSocketLambdaIntegration(
+            'DefaultIntegration',
+            lambdaChat
+          ),
+        },
+      }
+    );
 
-              const stage = new apigatewayv2.WebSocketStage(this, 'chatStage', {
-                webSocketApi,
-                stageName: 'prod',
-                autoDeploy: true,
-              });
+    const stage = new apigatewayv2.WebSocketStage(this, 'chatStage', {
+      webSocketApi,
+      stageName: 'prod',
+      autoDeploy: true,
+    });
 
-              //RUTA EJECUTADA CUANDO EL CLIENTE ENVIA UN MENSAJE
-              webSocketApi.addRoute('sendMessage', {
-                integration: new WebSocketLambdaIntegration('SendMessageIntegration', lambdaChat),
-              });
+    //RUTA EJECUTADA CUANDO EL CLIENTE ENVIA UN MENSAJE
+    webSocketApi.addRoute('sendMessage', {
+      integration: new WebSocketLambdaIntegration(
+        'SendMessageIntegration',
+        lambdaChat
+      ),
+    });
 
-              //SE CREAN PERMISOS PARA QUE LA FUNCION LAMBDA PUEDA MANEJAR LAS CONEXIONES
-              const policy = new iam.PolicyStatement({
+    //SE CREAN PERMISOS PARA QUE LA FUNCION LAMBDA PUEDA MANEJAR LAS CONEXIONES
+    /*  const policy = new iam.PolicyStatement({
                 effect: iam.Effect.ALLOW,
                 actions: ['execute-api:ManageConnections'],
                 resources: [
@@ -71,17 +90,46 @@ import { join } from "node:path";
                     //ACCOUNT
                   `arn:aws:execute-api:"us-east-1":"339712893600":${webSocketApi.apiId}/${stage.stageName}/*`
                 ],
-              });
+              }); */
 
-              lambdaChat.addToRolePolicy(policy);
+    const policy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['execute-api:ManageConnections'],
+      resources: [
+        this.formatArn({
+          service: 'execute-api',
+          resourceName: `${stage.stageName}/POST/@connections/*`,
+          resource: webSocketApi.apiId,
+        }),
+      ],
+    });
 
-              // Mostrar la URL del WebSocket en la salida
-              new CfnOutput(this, 'WebSocketURL', {
-                value: stage.url,
-                description: 'URL del WebSocket',
-              });
+    lambdaChat.addToRolePolicy(policy);
 
+    // Otorgar permisos adicionales a la función Lambda para manejar conexiones
+    webSocketApi.grantManageConnections(lambdaChat);
 
-        }
+    // Agregar política de recursos para permitir conexiones desde cualquier origen
+    const resourcePolicy = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          principals: [new iam.AnyPrincipal()],
+          actions: ['execute-api:Invoke'],
+          resources: ['execute-api:/*/*/*'],
+        }),
+      ],
+    });
 
-    }
+    // Aplicar la política de recursos al API
+    const cfnWebSocketApi = webSocketApi.node
+      .defaultChild as apigatewayv2.CfnApi;
+    cfnWebSocketApi.addPropertyOverride('PolicyDocument', resourcePolicy);
+
+    // Mostrar la URL del WebSocket en la salida
+    new CfnOutput(this, 'WebSocketURL', {
+      value: stage.url,
+      description: 'URL del WebSocket',
+    });
+  }
+}
